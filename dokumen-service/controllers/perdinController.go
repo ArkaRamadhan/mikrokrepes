246e6f5bd97c1722bf9d4b72a3ffe9184f11ca57
Arkha Rmhdn <arkaramadhan1933@gmail.com>
2024-10-22 14:44:56 +0700

    arka

--- dokumen-service/controllers/perdinController.go
+++ dokumen-service/controllers/perdinController.go
@@ -9,11 +9,11 @@ import (
 	"net/url"
 	"os"
 	"path/filepath"
-	"project-its/initializers"
-	"project-its/models"
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/arkaramadhan/its-vo/models"
 
 	"github.com/gin-gonic/gin"
 	"github.com/xuri/excelize/v2"
