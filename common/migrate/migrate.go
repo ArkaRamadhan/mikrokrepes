eb42821273b44da8c017fd2f21926f40f8dd9f87
Arkha Rmhdn <arkaramadhan1933@gmail.com>
2024-10-24 13:02:42 +0700

    update loadEnv

---
+++ common/migrate/migrate.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/arkaramadhan/its-vo/common/initializers"
+	"github.com/arkaramadhan/its-vo/common/models"
+)
+
+func init() {
+
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDB("informasi")
+	initializers.DB.Exec("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";")
+
+}
+
+func main() {
+
+	initializers.DB.AutoMigrate(
+		&models.File{},
+		&models.Notification{},
+	)
+
+}
